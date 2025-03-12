e8fc8d55e801253011bdc84abc4166dfe3ce5dc1
Idah Lindiwe Khumalo <86086701+Shantralee-58@users.noreply.github.com>
2025-03-12 14:31:05 +0200

    A simple calculator

---
+++ week1.py
@@ -0,0 +1,27 @@
+# Addition numbers
+num_1 = int (input("Please enter the first number to add: "))
+num_2 = int (input("Please enter the second number to add: "))
+
+sum_num = num_1 + num_2
+print(sum_num)
+
+# Product/ Multiplication
+num_1 = int (input("Please enter the first number to multiply: "))
+num_2 = int (input("Please enter the second number to multiply: "))
+
+product_num = num_1 * num_2
+print(product_num)
+
+# Subtraction
+num_1 = int (input("Please enter the first number to subtract: "))
+num_2 = int (input("Please enter the second number to subtract: "))
+
+diff_num = num_1 - num_2
+print(diff_num)
+
+#Division
+num_1 = int (input("Please enter the first number to divide: "))
+num_2 = int (input("Please enter the second number to divide: "))
+
+div_num = num_1 / num_2
+print(div_num)
